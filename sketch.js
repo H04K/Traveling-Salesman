2600c3f9899d1278f288d3f1d5567b5e0fe7cff1
Diplo <t.hesters@protonmail.com>
2020-05-10 17:39:04 +0200

    Color

--- sketch.js
+++ sketch.js
@@ -16,8 +16,8 @@ function setup() {
 
 
 function draw() {
-    
-    
+
+
   if (villes.length > 2) {
     test++
     d = calc(villes);
@@ -38,13 +38,14 @@ function draw() {
 
     //villes[i].show();
     ellipse(villes[i].x, villes[i].y, 8, 8)
+
   }
   beginShape();
   for (let i = 0; i < villes.length; i++) {
     vertex(villes[i].x, villes[i].y)
     noFill();
   }
-  
+
   endShape();
   stroke(255, 0, 255);
   strokeWeight(4);
@@ -58,29 +59,30 @@ function draw() {
   let y = floor(random(villes.length));
   let j = floor(random(villes.length));
   mel(villes, y, j);
-  
+
   noStroke();
- textSize(20)
-  fill(255);
-  
-text("Plus court  =  " + arrondir(maxDistance,4),11, 700);
-text('Distance en test = ' + arrondir(d,4),11,650)
+  textSize(20)
 
-text('Nombre de test  = ' + test,1000,700)
-text('Nombre de point  = ' + villes.length,1000,650)
+  fill(0, 153, 0)
+  text("Plus court  =  " + arrondir(maxDistance, 4), 11, 700);
+  fill(204, 0, 0)
+  text('Distance en test = ' + arrondir(d, 4), 11, 650)
+  fill(255)
+  text('Nombre de test  = ' + test, 1000, 700)
+  text('Nombre de point  = ' + villes.length, 1000, 650)
 
 
 
 }
 
-function arrondir(nombre, dec)
-{
+function arrondir(nombre, dec) {
   let nombre_signe = nombre >= 0 ? 1 : -1;
-  return (Math.round((nombre*Math.pow(10,dec))+(nombre_signe*0.0001))/Math.pow(10,dec)).toFixed(dec);
+  return (Math.round((nombre * Math.pow(10, dec)) + (nombre_signe * 0.0001)) / Math.pow(10, dec)).toFixed(dec);
 }
+
 function mousePressed() {
 
-  test =0;
+  test = 0;
   let b = new Ville(mouseX, mouseY, 50);
   villes.push(b);
   d = calc(villes);
